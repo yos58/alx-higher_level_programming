18a928ecedde129753c797731bdfb44ddbe9f9c1
yos58 <ywondimu82@gmail.com>
2021-11-16 21:29:05 +0300

    first 0x01. Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def islower(c):
+        c = ord(c)
+            if 97 <= c <= 122:
+                        return True
+            else:
+                        return False
+                    
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+        if number < 0:
+                    number = (number * -1) % 10
+        else:
+                    number = number % 10
+                        print(number, end="")
+                            return (number)
+                        
