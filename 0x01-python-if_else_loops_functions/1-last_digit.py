4be4396529ebf8474eba2ad2e0535a8a28a1990e
yos58 <ywondimu82@gmail.com>
2021-11-16 23:19:55 +0300

    first 0x01. Python - if/else, loopscis1g23356 last12

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    remainder = number % -10
+    last = -number % 10 * -1
 else:
-    remainder = number % 10
-    print('Last digit of', number, 'is', remainder, end=' ')
-    if remainder > 5:
-        print('and is greater than 5')
-    elif remainder == 0:
-        print('and is 0')
+    last = number % 10
+    if last == 0:
+        print('Last digit of {:d} is {:d} and is 0'.format(number, last))
+    elif last > 5:
+        print('Last digit of {:d} is {:d} and is greater than 5'
+              .format(number, last))
     else:
-        print('and is less than 6 and not 0')
+        print('Last digit of {:d} is {:d} and is less than 6 and not 0'
+              .format(number, last))
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
             for c in str:
-                        if ord('a') <= ord(c) <= ord('z'):
-                                    c = chr(ord(c) - 32)
-                                    print("{:s}".format(c), end='')
-                                    print()
+                        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+                                    c = chr(ord(c) - ord(' '))
+                                    print('{:s}'.format(c), end='')
+                                    print('\n', end='')
