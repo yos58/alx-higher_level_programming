562b1ddad572ba35baf24caf6537b40079609aba
yos58 <ywondimu82@gmail.com>
2021-11-18 21:11:25 +0300

    first commit 0x02. Python - import & modules1final34

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    return 98 + a ** b
-#!/usr/bin/python3
-
-
-def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-            return (c)
-        else:
-            return (sub(a, b))
+if __name__ == "__main__":
+    from sys import argv
+    add = 0
+    for s in argv[1:]:
+        add += int(s)
+        print("{:d}".format(add))
